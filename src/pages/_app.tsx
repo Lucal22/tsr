9906628f1c39542d91372310ca9d377d3c285cad
Luis Carlos <lucaldev22@gmail.com>
2022-08-05 13:28:32 -0300

    Theme switch storage complete.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,30 @@ import Header from '../components/Header';
 import { GlobalStyles } from '../styles/global-styles';
 import light from '../styles/theme/light';
 import dark from '../styles/theme/dark';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+//import useLocalTheme from '../utils/useLocalTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState(light);
 
-  const handleTheme = () => setTheme(theme.title === 'light' ? dark : light);
+  useEffect(() => {
+    const typeTheme = localStorage.getItem('theme');
+    if (typeTheme != 'light') {
+      setTheme(dark);
+    } else {
+      setTheme(light);
+    }
+  }, []);
+  const handleTheme = () => {
+    if (theme.title === 'light') {
+      setTheme(dark);
+      localStorage.setItem('theme', 'dark');
+    } else {
+      setTheme(light);
+      localStorage.setItem('theme', 'light');
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Header handleTheme={handleTheme} />
