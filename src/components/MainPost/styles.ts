be4b41aab0cb946108121066ce25f3cf4e4dfe5e
Luis Carlos <lucaldev22@gmail.com>
2023-02-23 17:56:13 -0300

    minor changes

--- src/components/MainPost/styles.ts
+++ src/components/MainPost/styles.ts
@@ -8,8 +8,8 @@ export const Container = styled.article`
     width: 100%;
     min-height: 30rem;
     background-color: ${theme.background.secondary};
-    border-radius: 5px;
-    padding: 20px 20px 10px 20px;
+    border-radius: 0.5rem;
+    padding: 2rem 2rem 1rem 2rem;
     color: ${theme.colors.primary};
     @media (min-width: ${theme.screen.size.small}) {
       height: 46rem;
