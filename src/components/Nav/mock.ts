de62d4d01ccf91cc7624a8976003625567257233
Luis Carlos <lucaldev22@gmail.com>
2023-04-10 17:16:00 -0300

    updating header components.

--- src/components/Nav/mock.ts
+++ src/components/Nav/mock.ts
@@ -2,108 +2,49 @@ const list = [
   [
     {
       children: 'Posts',
-      link: '/posts',
+      link: '/posts/page/1',
       newTab: false,
     },
     {
-      children: 'Guias',
-      link: '/guides',
+      children: 'Categorias',
+      link: '/categories',
       newTab: false,
-      topics: [
-        {
-          children: 'Profissões',
-          link: '/jobs',
-          newTab: false,
-        },
-        {
-          children: 'Montarias',
-          link: '/mounts',
-          newTab: false,
-        },
-        {
-          children: 'Deep dungeons',
-          link: '/ddungeons',
-          newTab: false,
-        },
-        {
-          children: 'Normal raids',
-          link: '/nraids',
-          newTab: false,
-        },
-        {
-          children: 'Colecionáveis',
-          link: '/nraids',
-          newTab: false,
-        },
-        {
-          children: 'Outros',
-          link: '/nraids',
-          newTab: false,
-        },
-      ],
     },
     {
-      children: 'Expansões',
-      link: '/expansions',
+      children: 'Sobre',
+      link: '/about',
       newTab: false,
-      topics: [
-        {
-          children: 'Endwalker',
-          link: '/expansions/endwalker/1',
-          newTab: false,
-        },
-        {
-          children: 'Shadowbringers',
-          link: '/expansions/shadowbringers/1',
-          newTab: false,
-        },
-        {
-          children: 'Stormblood',
-          link: '/expansions/stormblood/1',
-          newTab: false,
-        },
-        {
-          children: 'Heavensward',
-          link: '/expansions/heavensward/1',
-          newTab: false,
-        },
-        {
-          children: 'A realm reborn',
-          link: '/expansions/arr/1',
-          newTab: false,
-        },
-      ],
     },
   ],
   [
     {
       children: 'Endwalker',
-      link: '/endwalker',
+      link: '/expansions/endwalker/1',
       newTab: false,
     },
     {
       children: 'Shadowbringers',
-      link: '/shadowbringers',
+      link: '/expansions/shadowbringers/1',
       newTab: false,
     },
     {
       children: 'Stormblood',
-      link: '/stormblood',
+      link: '/expansions/stormblood/1',
       newTab: false,
     },
     {
       children: 'Heavensward',
-      link: '/heavensward',
+      link: '/expansions/heavensward/1',
       newTab: false,
     },
     {
       children: 'A realm reborn',
-      link: '/arr',
+      link: '/expansions/arr/1',
       newTab: false,
     },
     {
-      children: 'Live letter',
-      link: '/liveletter',
+      children: 'Brotinho',
+      link: 'posts/sproutspot/1',
       newTab: false,
     },
   ],
