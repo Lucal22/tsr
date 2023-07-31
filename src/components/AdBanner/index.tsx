e40f5629dec6973ffaaebd8dd484ba3a55555fb7
Luis Carlos <lucaldev22@gmail.com>
2023-07-31 13:20:16 -0300

    feat: Adding images component to the adbanner component.

--- src/components/AdBanner/index.tsx
+++ src/components/AdBanner/index.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image';
 import React from 'react';
 import { img } from '../../assets';
 import Container from '../Container';
-import Links from '../Links';
 import * as Styled from './styles';
+import Images from '../Images';
 
 export type AdBannerProps = {
   children: React.ReactNode;
@@ -13,9 +12,13 @@ export default function AdBanner() {
   return (
     <Container width={'large'}>
       <Styled.Banner>
-        <Links link={'/'}>
-          <Image src={img.banner} width="800px" height="300" />
-        </Links>
+        <Images
+          link={'/'}
+          src={img.banner}
+          width="800px"
+          height="300px"
+          alt={''}
+        />
       </Styled.Banner>
     </Container>
   );
