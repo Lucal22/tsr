308229d0f4e4a614d36ec4ee6ac19c13e694f911
Luis Carlos <lucaldev22@gmail.com>
2023-07-31 12:52:39 -0300

    feat: Adding Post component to the pages.

--- src/container/Posts/index.tsx
+++ src/container/Posts/index.tsx
@@ -4,23 +4,16 @@ import Grid from '../../components/Grid';
 import * as Styled from './styles';
 import SideContent from '../../components/SideContent';
 import { PageProps } from '../../Types/post';
-import Pagination from '../../components/Pagination';
+import PostCardDisplay from '../../components/PostCardDisplay';
 
-export default function Posts({
-  posts,
-  letter,
-  mount,
-
-}: PageProps) {
+export default function Posts({ posts, letter, mount, filter }: PageProps) {
   return (
     <Styled.Container>
       <Container width={'large'}>
         <Styled.Content>
           <Grid>
             <Styled.Posts>
-              <Pagination
-                posts={posts}
-              />
+              <PostCardDisplay posts={posts} filter={filter} />
             </Styled.Posts>
             <SideContent mount={mount} letter={letter} />
           </Grid>
