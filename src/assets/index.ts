3e7fa30b9d9527dca2976e6813a6e74265da4d28
lucaldev <lucaldev22@gmail.com>
2023-08-09 17:13:36 -0300

    feat: Adding img for about page.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,11 @@
 import logo from './img/Logo.png';
 import banner from './img/banner-contato.jpg';
 import footer from './img/footer-logo.png';
+import aboutBanner from './img/ffxiv-endwalker-ending.png';
+
 export const img = {
   logo,
   banner,
+  aboutBanner,
   footer,
 };
